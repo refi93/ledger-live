53f406626c67a823b7c7137aee28d9987b930c39
Olivier Freyssinet <olivier.freyssinet@ledger.fr>
2022-12-01 17:33:55 +0100

    Disabled style in PostOnboardingActionRow

--- apps/ledger-live-mobile/src/components/PostOnboarding/PostOnboardingActionRow.tsx
+++ apps/ledger-live-mobile/src/components/PostOnboarding/PostOnboardingActionRow.tsx
@@ -55,9 +55,13 @@ const PostOnboardingActionRow: React.FC<Props> = props => {
         alignItems="center"
         py={6}
         justifyContent="space-between"
+        opacity={disabled ? 0.5 : 1}
       >
         <Flex flexDirection="row" alignItems="center" flexShrink={1}>
-          <Icon size={24} color={completed ? "neutral.c70" : "primary.c80"} />
+          <Icon
+            size={24}
+            color={completed || disabled ? "neutral.c70" : "primary.c80"}
+          />
           <Flex ml={6} flexDirection="column" justifyContent="center" flex={1}>
             <Flex flexDirection="row" alignItems="center">
               <Text
@@ -87,7 +91,7 @@ const PostOnboardingActionRow: React.FC<Props> = props => {
           flexGrow={1}
           pl={6}
         >
-          {completed ? (
+          {disabled ? null : completed ? (
             <Icons.CheckAloneMedium color="success.c100" size={20} />
           ) : (
             <Icons.ChevronRightMedium color="neutral.c70" size={24} />
