9318fed1f9845a58b07ecc3a0a004620ca181566
Alexandre Magaud <alexandre.magaud@gmail.com>
2022-12-12 16:49:14 +0100

    feat: cli for new task getDeviceInfo

--- apps/cli/src/commands-index.ts
+++ apps/cli/src/commands-index.ts
@@ -36,6 +36,7 @@ import i18n from "./commands/i18n";
 import liveData from "./commands/liveData";
 import makeCompoundSummary from "./commands/makeCompoundSummary";
 import managerListApps from "./commands/managerListApps";
+import newGetDeviceInfo from "./commands/newGetDeviceInfo";
 import portfolio from "./commands/portfolio";
 import proxy from "./commands/proxy";
 import receive from "./commands/receive";
@@ -94,6 +95,7 @@ export default {
   liveData,
   makeCompoundSummary,
   managerListApps,
+  newGetDeviceInfo,
   portfolio,
   proxy,
   receive,
